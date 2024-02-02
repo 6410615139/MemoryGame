b1201c014de0a07758c5c0c3cc34e2d99f6ea9fb
Krit <6410615139@student>
2024-02-02 12:07:46 +0700

    Memory game all

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,15 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel = EmojiViewModel()
+    
     var body: some View {
-        RoundedRectangle(cornerRadius: 12)
-            .strokeBorder(lineWidth: 2)
-            .foregroundColor(.orange)
-            .padding()
-        Text("🎃")
+    VStack {
+        LazyVGrid(columns: 
+            [GridItem(), GridItem(),GridItem(), GridItem()]) {
+            ForEach(viewModel.cards) {card in
+                CardView(card)
+                    .aspectRatio(2/3, contentMode: .fit)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
+            }
+        }
+        .foregroundColor(.pink)
+        Spacer()
+    }
+    .padding()
     }
 }
 
+struct CardView: View {
+    var card: MemoryGameModel<String>.Card
+    init (_ card: MemoryGameModel<String>.Card) {
+        self.card = card
+    }
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(card.content)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            
+            base.opacity(card.isFaceUp ? 0 : 1)
+        }
+    }
+    
+}
 #Preview {
     ContentView()
 }
